3d9153251c4b9ac74110aa7732b755dd30d19219
Anna Sokolova <annasprite555@gmail.com>
2024-03-13 01:16:11 +0200

    delete unnecessary files

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const luckyBtn = document.getElementById("luckyBtn");
-const googleLogo = document.querySelector(".logo--google");
-const oddityLogo = document.querySelector(".logo--oddity");
-luckyBtn.addEventListener("click", (event) => {
-  event.preventDefault;
-  googleLogo.classList.toggle("display-none");
-  oddityLogo.classList.toggle("display-none");
+document.addEventListener("DOMContentLoaded", () => {
+  const luckyBtn = document.getElementById("luckyBtn");
+  const googleLogo = document.querySelector(".logo--google");
+  const oddityLogo = document.querySelector(".logo--oddity");
+  luckyBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    googleLogo.classList.toggle("display-none");
+    oddityLogo.classList.toggle("display-none");
+  });
 });
-console.log(luckyBtn);
