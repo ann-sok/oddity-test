be6dd2d22fd56f928f2c45c431b95eee23ee599f
Anna Sokolova <annasprite555@gmail.com>
2024-03-13 13:10:50 +0200

    change class names

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   const luckyBtn = document.getElementById("luckyBtn");
-  const googleLogo = document.querySelector(".logo--google");
-  const oddityLogo = document.querySelector(".logo--oddity");
-  luckyBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    googleLogo.classList.toggle("display-none");
-    oddityLogo.classList.toggle("display-none");
-  });
+  const logoImg = document.querySelector(".logo__img");
+  if (logoImg.dataset.altSrc) {
+    luckyBtn.addEventListener("click", (event) => {
+      event.preventDefault();
+      const currentSrc = [logoImg.src, logoImg.srcset, logoImg.alt];
+      const altSrc = [
+        logoImg.dataset.altSrc,
+        logoImg.dataset.altSrcset,
+        logoImg.dataset.altAlt,
+      ];
+      logoImg.src = altSrc[0];
+      logoImg.srcset = altSrc[1];
+      logoImg.alt = altSrc[2];
+      logoImg.dataset.altSrc = currentSrc[0];
+      logoImg.dataset.altSrcset = currentSrc[1];
+      logoImg.dataset.altAlt = currentSrc[2];
+    });
+  }
 });
